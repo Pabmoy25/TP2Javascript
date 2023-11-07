ad1182b6525d39798aee399fc03186ea932049ec
Pablo Moyano <pabmoy1725@gmail.com>
2023-11-07 07:20:50 -0300

    Se agrega ejercicio 12 resuelto

---
+++ js/12-numeroAleatorio.js
@@ -0,0 +1,9 @@
+// 12- Realiza un script que genere un número aleatorio entre 1 y 99
+
+do {
+    let numAleatorio = Math.floor((Math.random() * 99) + 1);
+    document.write(`<h2>El número aleatorio es: ${numAleatorio}</h2>`);
+    
+    } while (numAleatorio);
+    
+
